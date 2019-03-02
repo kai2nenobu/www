69bc0e93c90cd0b78f6ba259fe5ba1034b740714
Tsunenobu Kai <kai2nenobu@gmail.com>
2019-03-02 18:28:55 +0900

    変更をGitHubにpushする処理をスクリプトにまとめる

---
+++ script/push_changes_to_github.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+GITHUB_DEPLOY_KEY=$(mktemp)
+trap 'clean_up' EXIT
+
+clean_up() {
+  rm -f "$GITHUB_DEPLOY_KEY" || true
+}
+
+## 変更があればコミットする
+git diff
+git add content
+git -c user.name="Travis CI" -c user.email="travis@travis-ci.org" commit -m "[ci skip] Update documents generated from ${TRAVIS_COMMIT}"
+
+## デプロイキーを復号する
+openssl aes-256-cbc -K $encrypted_c546895bd2a4_key -iv $encrypted_c546895bd2a4_iv -in .travis/www_deploy_key_by_travis.enc -out "$GITHUB_DEPLOY_KEY" -d
+
+## sshを使ってpushするようにURLを変更する
+git config url."git@github.com:".pushInsteadOf "https://github.com/"
+git remote -v
+
+## デプロイキーを使ってpushする
+export GIT_SSH_COMMAND="ssh -i ${GITHUB_DEPLOY_KEY}"
+git push origin "HEAD:${TRAVIS_BRANCH}" >/dev/null 2>&1

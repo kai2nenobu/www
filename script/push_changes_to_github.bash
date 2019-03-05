e41ce3c35e90d3e90af31ce3e8e160befc7fac03
Tsunenobu Kai <kai2nenobu@gmail.com>
2019-03-05 12:54:13 +0900

    content以下にdiffがある場合のみプッシュする
    content以下に変更がなければプッシュする必要も、秘密鍵を
    復号する必要もないので、ちゃんと条件分岐するようにしました。

--- script/push_changes_to_github.bash
+++ script/push_changes_to_github.bash
@@ -21,19 +21,22 @@ script_echo() {
   echo "[${SCRIPT_NAME}]\$" "$@"
 }
 
-## 変更があればコミットする
-git diff
-git add content
-doing git -c user.name="Travis CI" -c user.email="travis@travis-ci.org" commit -m "[ci skip] Update documents generated from ${TRAVIS_COMMIT:0:7}"
+## contentに変更があるか確認する
+doing git diff -- content
+if [ "$(git diff --name-only -- content) | wc -l" -gt 0 ]; then
+  ## 変更をコミットする
+  git add content
+  doing git -c user.name="Travis CI" -c user.email="travis@travis-ci.org" commit -m "[ci skip] Update documents generated from ${TRAVIS_COMMIT:0:7}"
 
-## デプロイキーを復号する
-script_echo openssl aes-256-cbc でデプロイキーを復号する
-openssl aes-256-cbc -K $encrypted_c546895bd2a4_key -iv $encrypted_c546895bd2a4_iv -in .travis/www_deploy_key_by_travis.enc -out "$GITHUB_DEPLOY_KEY" -d
+  ## デプロイキーを復号する
+  script_echo openssl aes-256-cbc でデプロイキーを復号する
+  openssl aes-256-cbc -K $encrypted_c546895bd2a4_key -iv $encrypted_c546895bd2a4_iv -in .travis/www_deploy_key_by_travis.enc -out "$GITHUB_DEPLOY_KEY" -d
 
-## sshを使ってpushするようにURLを変更する
-git config url."git@github.com:".pushInsteadOf "https://github.com/"
-doing git remote -v
+  ## sshを使ってpushするようにURLを変更する
+  git config url."git@github.com:".pushInsteadOf "https://github.com/"
+  doing git remote -v
 
-## デプロイキーを使ってpushする
-export GIT_SSH_COMMAND="ssh -i ${GITHUB_DEPLOY_KEY}"
-doing git push origin "HEAD:${TRAVIS_BRANCH}"
+  ## デプロイキーを使ってpushする
+  export GIT_SSH_COMMAND="ssh -i ${GITHUB_DEPLOY_KEY}"
+  doing git push origin "HEAD:${TRAVIS_BRANCH}"
+fi
